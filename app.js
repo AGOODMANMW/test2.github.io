70048306e07b4b08254730869ec9e7e6a69d5d00
AGOODMANMW <ksr2007@yandex.ru>
2024-08-26 20:55:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let tg = window.Telegram.WebApp;
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
+
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
